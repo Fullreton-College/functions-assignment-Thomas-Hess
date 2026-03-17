dc4f64f78cb5706d9fb290ae9a99625867dfb8de
Thomas-Hess <2degreesummit@gmail.com>
2026-03-16 20:03:38 -0700

    Update Function_1.cpp
    Created by Thomas Hess

--- Function_1.cpp
+++ Function_1.cpp
@@ -2,14 +2,35 @@
 #include <cstdlib>
 using namespace std;
 
-/* Define your function here */ 
+// returns "Heads" or "Tails"
+string CoinFlip() {
+    int value = rand() % 2;   // random number 0 or 1
+
+    if (value == 1) {
+        return "Heads";
+    }
+    else {
+        return "Tails";
+    }
+}
 
 int main() {
-   // Add more variables as needed
-   
-   srand(2);  // Unique seed
+    int numFlips;
+    
+    cout << "How many times would you like to "
+         << "flip a coin: ";
+    cin >> numFlips;
+    srand(2); // unique seed
 
-   /* Type your code here */
+    for (int i = 0; i < numFlips; i++) {
+        cout << CoinFlip();
+        
+        if (i < numFlips - 1) {
+            cout << " ";
+        }
+    }
 
-   return 0;
+    cout << endl;
+
+    return 0;
 }

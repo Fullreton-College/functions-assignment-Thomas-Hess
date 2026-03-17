3663f4d1caebc8d246b115663b60c395b80b14aa
Thomas-Hess <2degreesummit@gmail.com>
2026-03-16 22:01:36 -0700

    Update Function_3.cpp
    Created by Thomas Hess

--- Function_3.cpp
+++ Function_3.cpp
@@ -1,46 +1,61 @@
 #include <iostream>
-using namspace std;
+using namespace std;
 
-void input( int& hours24, int& minutes);
-void output( int hours, int minutes);
+void input(int& hours24, int& minutes);
+void output(int hours, int minutes);
 
-int main()
-{
-    int timeHours, timeMinutes, waitHours, waitMinutes,finishHours, finishMinutes;
+int main() {
+    int timeHours, timeMinutes, waitHours, waitMinutes;
+    int finishHours, finishMinutes;
 
-  cout << "Compute completion time from current time and waiting period\n";
-  
+    cout << "Compute completion time from current time and waiting period\n";
 
-  char ans = 'y'; 
-  while ('y' == ans || 'Y' == ans)
-  {
-    cout << "Current time:\n";
-    input(timeHours, timeMinutes);
+    char ans = 'y';
+    while (ans == 'y' || ans == 'Y') {
+        cout << "Current time:\n";
+        input(timeHours, timeMinutes);
 
-    cout << "Waiting time:\n";
-    input(waitHours, waitMinutes);
+        cout << "Waiting time:\n";
+        input(waitHours, waitMinutes);
 
-    
-    finishHours = timeHours + waitHours;
-    finishMinutes = timeMinutes + waitMinutes;
+        // compute finish time
+        finishHours = timeHours + waitHours;
+        finishMinutes = timeMinutes + waitMinutes;
 
-    
-    finishHours += finishMinutes / 60;
-    if(finishHours >= 24)
-	{
-	  finishHours %= 24;
-	  cout << "Completion time is in the day following the start time\n";
+        // extra minutes converted to hours
+        finishHours += finishMinutes / 60;
+        finishMinutes %= 60;
+
+        // if there is a 24-hour wrap:
+        if (finishHours >= 24) {
+            finishHours %= 24;
+            cout << "Completion time is in the day following the start time\n";
+        }
+
+        cout << "Completion time: ";
+        output(finishHours, finishMinutes);
+
+        cout << "\n\nEnter Y or y to continue, any other halts\n\n";
+        cin >> ans;
     }
 
-    finishMinutes%= 60;
-
-    cout << "Completion ";
-    output(finishHours, finishMinutes);
-
-	cout << "\n\nEnter Y or y to continue, any other halts\n\n";
-	cin >> ans;
-  }
-
-  return 0;
+    return 0;
 }
 
+// input time
+void input(int& hours24, int& minutes) {
+    cout << "Enter hours (0-23): ";
+    cin >> hours24;
+
+    cout << "Enter minutes (0-59): ";
+    cin >> minutes;
+}
+
+// output time in HH:MM format
+void output(int hours, int minutes) {
+    if (hours < 10) cout << "0";
+    cout << hours << ":";
+
+    if (minutes < 10) cout << "0";
+    cout << minutes;
+}

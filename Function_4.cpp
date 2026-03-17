12408df32b1a85cfe39158c7317d6ca682fbff0f
Thomas-Hess <2degreesummit@gmail.com>
2026-03-16 21:49:57 -0700

    Update Function_4.cpp
    Created by Thomas Hess

--- Function_4.cpp
+++ Function_4.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
 using namespace std;
 
-//define necessary functions
-int main()
-{
+// prototype
+void computeCoins(int coinValue, int& num, int& amountLeft);
 
-  //your code goes here
+int main() {
+    int amount;
 
-return 0;
+    cout << "Enter amount of change (1–99 cents): ";
+    cin >> amount;
+
+    if (amount < 1 || amount > 99) {
+        cout << "Invalid amount." << endl;
+        return 0;
+    }
+
+    int amountLeft = amount;
+    int quarters, dimes, pennies;
+
+    // compute coins
+    computeCoins(25, quarters, amountLeft);
+    computeCoins(10, dimes, amountLeft);
+    computeCoins(1, pennies, amountLeft);
+
+    // result
+    cout << amount << " cents can be given as "
+         << quarters << " quarter(s) "
+         << dimes << " dime(s) and "
+         << pennies << " penny(pennies)" << endl;
+
+    return 0;
 }
 
-//implement functions
+// function
+void computeCoins(int coinValue, int& num, int& amountLeft) {
+    num = amountLeft / coinValue; // lowest int
+    amountLeft = amountLeft % coinValue;
+}
